c2c1a97021de662b325965f40781314598fdf425
keyhong <wnhong96@gmail.com>
2024-01-06 02:01:59 +0900

    Site updated: 2024-01-06 02:01:58

--- libs/codeBlock/codeCopy.js
+++ libs/codeBlock/codeCopy.js
@@ -1,17 +1,17 @@
-// 代码块一键复制
+// 한 번의 클릭으로 코드 블록 복사
 
 $(function () {
-    var $copyIcon = $('<i class="fas fa-copy code_copy" title="复制代码" aria-hidden="true"></i>')
+    var $copyIcon = $('<i class="fas fa-copy code_copy" title="코드복사" aria-hidden="true"></i>')
     var $notice = $('<div class="codecopy_notice"></div>')
     $('.code-area').prepend($copyIcon)
     $('.code-area').prepend($notice)
-    // “复制成功”字出现
+    // "복사 성공"이라는 단어가 나타납니다.
     function copy(text, ctx) {
         if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
             try {
                 document.execCommand('copy') // Security exception may be thrown by some browsers.
                 $(ctx).prev('.codecopy_notice')
-                    .text("复制成功")
+                    .text("복사성공")
                     .animate({
                         opacity: 1,
                         top: 30
@@ -25,7 +25,7 @@ $(function () {
                     })
             } catch (ex) {
                 $(ctx).prev('.codecopy_notice')
-                    .text("复制失败")
+                    .text("복사실패")
                     .animate({
                         opacity: 1,
                         top: 30
